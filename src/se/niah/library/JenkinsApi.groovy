b36d3b6cb501b124e80faa4d408bf99b157f69b0
Niclas Ahlstrand <niclas.ahlstrand@gmail.com>
2020-11-15 22:54:25 +0100

    testing

--- src/se/niah/library/JenkinsApi.groovy
+++ src/se/niah/library/JenkinsApi.groovy
@@ -2,7 +2,7 @@ package se.niah.library
 
 import hudson.model.Node
 
-class groovy implements Serializable {
+class JeninsApi implements Serializable {
     public static List<Node> getNodes() {
         return Jenkins.instance.getNodes()
     }
