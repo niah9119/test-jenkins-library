d1b1196e9cd801411525c4eda8b584d31a34eedc
Niclas Ahlstrand <niclas.ahlstrand@gmail.com>
2020-11-15 22:26:24 +0100

    testing

--- vars/superPipeline.groovy
+++ vars/superPipeline.groovy
@@ -1,10 +1,20 @@
-def call(Map config = ["name":"No Name Pipeline"], Closure body = {}) {
+hudson.model.Node
+hudson.model.Jenkins
+
+def call(Map config = ["name":"No Name Pipeline"]) {
     pipeline {
         agent { docker 'groovy' }
+        def nodes = ""
         stages {
             stage('Stage 1') {
+                script {
+
+                    p
+                                    }
                 steps {
                     sh '/home/jenkins/pipeline_scripts/hello.groovy'
+                    List<Nodes> nodeList = api
+                    echo "Nodes: " + nodeList
                 }
             }
         }
