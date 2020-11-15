5b79ab31827188ab9e51b4cfc4ca48cc64d2804d
Niclas Ahlstrand <niclas.ahlstrand@gmail.com>
2020-11-15 20:51:38 +0100

    testing

--- vars/superpipeline.groovy
+++ vars/superpipeline.groovy
@@ -1,6 +1,6 @@
 // vars/sayHello.groovy
 def call(Map config = ["name":"No Name Pipeline"], Closure body = {}) {
-    echo "Hello from pipeline: ${config.name}"
+    echo "Hello from superpipeline: ${config.name}"
     if ( body != null) {
         body()
     }
